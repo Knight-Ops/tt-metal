dbef486028730aa154bf9b1088992bb15dd0299e
Sohaib Nadeem <sohaibnadeem@tenstorrent.com>
2026-06-18 19:06:59 -0400

    Declare noc_mode as extern in dataflow_api_common.h

--- tt_metal/hw/inc/internal/dataflow/dataflow_api_common.h
+++ tt_metal/hw/inc/internal/dataflow/dataflow_api_common.h
@@ -12,6 +12,7 @@ constexpr uint8_t noc_mode = NOC_MODE;
 #else
 
 extern uint8_t noc_index;
+extern uint8_t noc_mode;
 // noc_mode may switch dynamically while in the firmware, so we can't define it here.
 #endif
 extern uint16_t dram_bank_to_noc_xy[NUM_NOCS][NUM_DRAM_BANKS];
