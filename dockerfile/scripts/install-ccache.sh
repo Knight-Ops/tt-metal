9e0299899ff745de6e6bb1c5c4455823b8a121ca
Wilder <wilder@tenstorrent.com>
2026-06-20 07:19:14 +0000

    chore: bump ccache-storage-s3-go to v0.1.2 (fix GET crash)

--- dockerfile/scripts/install-ccache.sh
+++ dockerfile/scripts/install-ccache.sh
@@ -12,10 +12,10 @@ CCACHE_SHA256="${CCACHE_SHA256:-508b2a1217dc6e04a23e967c7b95a0fb45d8a7e16fde9e18
 # Remote storage helper for S3-backed ccache (crsh: custom storage protocol).
 # ccache derives the helper name (ccache-storage-s3) from the s3:// URL scheme
 # and looks for it in its libexec_dirs (baked in as /usr/local/libexec below).
-STORAGE_HELPER_VERSION="${STORAGE_HELPER_VERSION:-0.1.1}"
-# SHA256 for ccache-storage-s3-go-0.1.1-linux-amd64.tar.gz
+STORAGE_HELPER_VERSION="${STORAGE_HELPER_VERSION:-0.1.2}"
+# SHA256 for ccache-storage-s3-go-0.1.2-linux-amd64.tar.gz
 # Verified by downloading from GitHub releases and running sha256sum.
-STORAGE_HELPER_SHA256="${STORAGE_HELPER_SHA256:-962e7089f186569811e9697420d7557b4ebf2dcf972b35c90c293ae08999892a}"
+STORAGE_HELPER_SHA256="${STORAGE_HELPER_SHA256:-f8302ffea0c0e6759ca938131ab144202b88d641f57cb5ba74587c3fa30afc5a}"
 
 INSTALL_DIR="${INSTALL_DIR:-/usr/local}"
 DOWNLOAD_URL="https://github.com/ccache/ccache/releases/download/v${CCACHE_VERSION}/ccache-${CCACHE_VERSION}-linux-x86_64-glibc.tar.xz"
